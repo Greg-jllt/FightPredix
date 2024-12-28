f7286c8356773b06a6e5966b7b1d4dcf6ac513ec
Greg-jllt <greg.jlt03@gmail.com>
2024-12-28 18:35:11 +0100

    rajout calcul ancienneté

--- FightPredix/__init__.py
+++ FightPredix/__init__.py
@@ -6,6 +6,6 @@ from .lib_ufc_stats import _cherche_combattant_UFC_stats
 
 from .lib_combats import _main_combat_recolte
 
-from .lib_constructeur import _difference_combats, _age_by_DOB
+from .lib_constructeur import _difference_combats, _age_by_DOB, _transformation_debut_octogone
 
 from .outils import configure_logger, lire_combattant_manqué, ecrire_combattant_manqué
