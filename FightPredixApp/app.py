5dcaf0ca778379052c6807b74fe54e367ba72eed
Greg-jllt <greg.jlt03@gmail.com>
2025-01-13 01:10:42 +0100

    streamlit avec modele

--- FightPredixApp/app.py
+++ FightPredixApp/app.py
@@ -7,12 +7,174 @@ import joblib
 import requests
 from PIL import Image
 import time
+import re
 
 
 from pages import page_predictions
+from rapidfuzz import fuzz
 from style import init_pages, bouton_prediction
+from datetime import datetime
 
 init_pages()
+
+
+def _calcul_nb_mois_dernier_combat(combats: pd.DataFrame) -> pd.DataFrame:
+    """
+    Fonction qui calcule les victoires des combattants au moment du combat
+    """
+    cob = combats.copy()
+    cob = cob.sort_index(ascending=False)
+
+    temp_dict: dict = {}
+
+    def _sub_nb_mois_dernier_combat(combattant, nickname, prefixe):
+        if f"{combattant}_{nickname}_date" not in temp_dict.keys():
+            temp_dict[f"{combattant}_{nickname}_date"] = []
+
+        temp_dict[f"{combattant}_{nickname}_date"].append(date)
+
+        if len(temp_dict[f"{combattant}_{nickname}_date"]) > 2:
+            temp_dict[f"{combattant}_{nickname}_date"].pop(0)
+
+        cob.loc[i, f"{prefixe}_nb_mois_dernier_combat"] = (
+            round(
+                (
+                    datetime.strptime(
+                        temp_dict[f"{combattant}_{nickname}_date"][1], "%Y-%m-%d"
+                    )
+                    - datetime.strptime(
+                        temp_dict[f"{combattant}_{nickname}_date"][0], "%Y-%m-%d"
+                    )
+                ).days
+                / 30
+            )
+            if len(temp_dict[f"{combattant}_{nickname}_date"]) == 2
+            else 0
+        )
+
+    for i, combat in cob.iterrows():
+        combattant_1, nickname_1 = combat["combattant_1"], combats["nickname_1"]
+        combattant_2, nickname_2 = combat["combattant_2"], combats["nickname_2"]
+        date = combat["date"]
+
+        nickname_1 = nickname_1 if isinstance(nickname_1, str) else "NO"
+        nickname_2 = nickname_2 if isinstance(nickname_2, str) else "NO"
+
+        _sub_nb_mois_dernier_combat(combattant_1, nickname_1, "combattant_1")
+        _sub_nb_mois_dernier_combat(combattant_2, nickname_2, "combattant_2")
+
+    cob = cob.sort_index(ascending=True)
+
+    return cob
+
+def _difference_num_combats(combats: pd.DataFrame) -> pd.DataFrame:
+    """
+    Fonction de calcul de la différence entre les caractéristiques des combattants
+    au sein de chaque combat.
+    """
+
+    cols_to_drop = []
+    colonnes_a_concat = {}
+
+    num_colonnes_combats = combats.select_dtypes(include=["number"]).columns
+
+    pattern = re.compile(r"combattant_(\d+)_(.+)")
+
+    for col in num_colonnes_combats:
+        match = pattern.match(col)
+        if match:
+            stat_type = match.group(2)
+            colonnes_a_concat[f"diff_{stat_type}"] = (
+                combats[f"combattant_1_{stat_type}"]
+                - combats[f"combattant_2_{stat_type}"]
+            )
+            cols_to_drop.append(f"combattant_1_{stat_type}")
+            cols_to_drop.append(f"combattant_2_{stat_type}")
+
+    df_numerique = pd.DataFrame(colonnes_a_concat, index=combats.index)
+
+    resultat = pd.concat([combats.reset_index(drop=True), df_numerique], axis=1)
+
+    # if cols_to_drop:
+    #     Console().print(f"Colonnes à supprimer : {len(cols_to_drop)}")
+    #     resultat.drop(cols_to_drop, axis=1, inplace=True)
+
+    return resultat
+
+
+
+def _liste_features() -> tuple[list[str], list[str], list[str]]:
+    """
+    Renvoie des listes de variables utilisables pour le machine learning
+    """
+
+    numeric_features = [
+        "diff_age_t",
+        "diff_win_t",
+        "diff_losses_t",
+        "diff_forme",
+        "diff_serie",
+        "diff_Sig_str_total_reussi_moyenne",
+        "diff_Sig_str_total_total_moyenne",
+        "diff_Total_str_total_reussi_moyenne",
+        "diff_Total_str_total_total_moyenne",
+        "diff_Tdtotal_reussi_moyenne",
+        "diff_Tdtotal_total_moyenne",
+        "diff_Headsig_str_total_reussi_moyenne",
+        "diff_Headsig_str_total_total_moyenne",
+        "diff_Bodysig_str_total_reussi_moyenne",
+        "diff_Bodysig_str_total_total_moyenne",
+        "diff_Legsig_str_total_reussi_moyenne",
+        "diff_Legsig_str_total_total_moyenne",
+        "diff_Distancesig_str_total_reussi_moyenne",
+        "diff_Distancesig_str_total_total_moyenne",
+        "diff_Clinchsig_str_total_reussi_moyenne",
+        "diff_Clinchsig_str_total_total_moyenne",
+        "diff_Groundsig_str_total_reussi_moyenne",
+        "diff_Groundsig_str_total_total_moyenne",
+        "diff_frappe_tete_moyenne",
+        "diff_frappe_corps_moyenne",
+        "diff_frappe_jambe_moyenne",
+        "diff_frappe_distance_moyenne",
+        "diff_frappe_clinch_moyenne",
+        "diff_frappe_sol_moyenne",
+        "diff_KDtotal_moyenne",
+        "diff_Sig_str_total_ratio_moyenne",
+        "diff_Sig_str_percent_total_moyenne",
+        "diff_Total_str_total_ratio_moyenne",
+        "diff_Tdtotal_ratio_moyenne",
+        "diff_Td_percent_total_moyenne",
+        "diff_Sub_atttotal_moyenne",
+        "diff_Revtotal_moyenne",
+        "diff_Ctrltotal_moyenne",
+        "diff_Headsig_str_total_ratio_moyenne",
+        "diff_Bodysig_str_total_ratio_moyenne",
+        "diff_Legsig_str_total_ratio_moyenne",
+        "diff_Distancesig_str_total_ratio_moyenne",
+        "diff_Clinchsig_str_total_ratio_moyenne",
+        "diff_Groundsig_str_total_ratio_moyenne",
+        "diff_la_taille",
+        "diff_poids",
+        "diff_reach",
+        "diff_portee_de_la_jambe",
+        "diff_nb_mois_dernier_combat",
+        "diff_DEC",
+        "diff_KO/TKO",
+        "diff_SUB",
+    ]
+
+    categorical_features = [
+        "combattant_1_style_de_combat",
+        "combattant_2_style_de_combat",
+        "combattant_1_country_of_residence_tapology",
+        "combattant_2_country_of_residence_tapology",
+        "combattant_1_country_of_birth_tapology",
+        "combattant_2_country_of_birth_tapology"
+    ]
+
+    output_feature = ["resultat", "poids_ml"]
+
+    return numeric_features, categorical_features, output_feature
 
 
 @st.cache_resource
@@ -133,7 +295,7 @@ elif st.session_state.current_page == "combattants":
     if "url_2" not in st.session_state:
         st.session_state["url_2"] = "None"
 
-    file_path = os.path.join("Data/Data_final_fighters.csv")
+    file_path = os.path.join("Data/Data_final_fighters_V.csv")
 
     a1,a2 = st.columns([0.5, 1])
 
@@ -327,6 +489,170 @@ elif st.session_state.current_page == "predictions":
     if st.session_state["predictable"] == False:
         titre("Choisissez deux combattants différents.")
     else:
+        DataFighters = pd.read_csv("Data/Data_final_fighters_V.csv", encoding="utf-8")
+        DataCombats = pd.read_csv("Data/Data_final_combats_V.csv", encoding="utf-8")
+
+        DataCombats = _calcul_nb_mois_dernier_combat(DataCombats)
+
+        num_features, cat_features, output_features = _liste_features()
+
+        predictions = []
+
+        DataCombats.rename(columns={"diff_portÃ©e_de_la_jambe": "diff_portee_de_la_jambe"}, inplace=True)
+        DataFighters.rename(columns={"portée_de_la_jambe": "portee_de_la_jambe", "âge": "age"}, inplace=True)  
+
+        indice_nom1 = DataFighters[DataFighters["name"] == st.session_state["fighter_1"]].index[0]
+        indice_nom2 = DataFighters[DataFighters["name"] == st.session_state["fighter_2"]].index[0]
+
+        for num1, num2 in zip((indice_nom1,indice_nom2), (indice_nom2,indice_nom1)):
+            Combattant_1 = pd.DataFrame(DataFighters.loc[num1]).T
+            Combattant_2 = pd.DataFrame(DataFighters.loc[num2]).T
+            
+            Combattant_1.columns = [
+                "combattant_1" + str(col) if "moyenne" in col else "combattant_1_" + str(col)
+                for col in Combattant_1.columns
+            ]
+            Combattant_2.columns = [
+                "combattant_2" + str(col) if "moyenne" in col else "combattant_2_" + str(col)
+                for col in Combattant_2.columns
+            ]
+            Combattant_1["join_key"] = "1"
+            Combattant_2["join_key"] = "1"
+            
+            combat = Combattant_1.merge(Combattant_2, on="join_key")
+            Data_combattant_1 = pd.DataFrame()
+            Data_combattant_2 = pd.DataFrame()
+            
+            nom1 = DataFighters.loc[num1, "name"]
+            nom2 = DataFighters.loc[num2, "name"]
+
+            for c1, c2 in zip(DataCombats["combattant_1"], DataCombats["combattant_2"]):
+                # Comparer avec le premier combattant
+                if nom1.lower() == c1.lower():
+                    Data_combattant_1 = pd.concat([Data_combattant_1, DataCombats[DataCombats["combattant_1"] == c1]])
+                elif fuzz.ratio(nom1.lower(), c1.lower()) >= 90:
+                    Data_combattant_1 = pd.concat([Data_combattant_1, DataCombats[DataCombats["combattant_1"] == c1]])
+                
+                if nom1.lower() == c2.lower():
+                    Data_combattant_1 = pd.concat([Data_combattant_1, DataCombats[DataCombats["combattant_2"] == c2]])
+                elif fuzz.ratio(nom1.lower(), c2.lower()) >= 90:
+                    Data_combattant_1 = pd.concat([Data_combattant_1, DataCombats[DataCombats["combattant_2"] == c2]])
+
+                # Comparer avec le seizième combattant
+                if nom2.lower() == c1.lower():
+                    Data_combattant_2 = pd.concat([Data_combattant_2, DataCombats[DataCombats["combattant_1"] == c1]])
+                elif fuzz.ratio(nom2.lower(), c1.lower()) >= 90:
+                    Data_combattant_2 = pd.concat([Data_combattant_2, DataCombats[DataCombats["combattant_1"] == c1]])
+                
+                if nom2.lower() == c2.lower():
+                    Data_combattant_2 = pd.concat([Data_combattant_2, DataCombats[DataCombats["combattant_2"] == c2]])
+                elif fuzz.ratio(nom2.lower(), c2.lower()) >= 90:
+                    Data_combattant_2 = pd.concat([Data_combattant_2, DataCombats[DataCombats["combattant_2"] == c2]])
+
+            Data_combattant_1.drop_duplicates(inplace=True)
+            Data_combattant_1.sort_index(inplace=True)
+            Data_combattant_2.drop_duplicates(inplace=True)
+            Data_combattant_2.sort_index(inplace=True)
+
+
+            if nom1.lower() == Data_combattant_1["combattant_1"].iloc[0].lower() or nom1.lower() == Data_combattant_1["combattant_2"].iloc[0].lower():
+                combattant_1_forme = Data_combattant_1["combattant_1_forme"].iloc[0]
+                combattant_1_serie = Data_combattant_1["combattant_1_serie"].iloc[0]
+                combattant_1_nb_mois = Data_combattant_1["combattant_1_nb_mois_dernier_combat"].iloc[0]
+            elif fuzz.ratio(nom1.lower(), Data_combattant_1["combattant_1"].iloc[0].lower()) >= 90 or fuzz.ratio(nom1.lower(), Data_combattant_1["combattant_2"].iloc[0].lower()) >= 90:
+                combattant_1_forme = Data_combattant_1["combattant_2_forme"].iloc[0]
+                combattant_1_serie = Data_combattant_1["combattant_2_serie"].iloc[0]
+                combattant_1_nb_mois = Data_combattant_1["combattant_2_nb_mois_dernier_combat"].iloc[0]
+                
+            if nom2.lower() == Data_combattant_2["combattant_1"].iloc[0].lower() or nom2.lower() == Data_combattant_2["combattant_2"].iloc[0].lower():
+                combattant_2_forme = Data_combattant_2["combattant_1_forme"].iloc[0]
+                combattant_2_serie = Data_combattant_2["combattant_1_serie"].iloc[0]
+                combattant_2_nb_mois = Data_combattant_2["combattant_1_nb_mois_dernier_combat"].iloc[0]
+            elif fuzz.ratio(nom2.lower(), Data_combattant_2["combattant_1"].iloc[0].lower()) >= 90 or fuzz.ratio(nom2.lower(), Data_combattant_2["combattant_2"].iloc[0].lower()) >= 90:
+                combattant_2_forme = Data_combattant_2["combattant_2_forme"].iloc[0]
+                combattant_2_serie = Data_combattant_2["combattant_2_serie"].iloc[0]
+                combattant_2_nb_mois = Data_combattant_2["combattant_2_nb_mois_dernier_combat"].iloc[0]
+                
+            combat.loc[0, "combattant_1_forme"] = combattant_1_forme
+            combat.loc[0, "combattant_1_serie"] = combattant_1_serie
+            combat.loc[0, "combattant_2_forme"] = combattant_2_forme
+            combat.loc[0, "combattant_2_serie"] = combattant_2_serie
+            combat.loc[0, "combattant_1_nb_mois_dernier_combat"] = combattant_1_nb_mois
+            combat.loc[0, "combattant_2_nb_mois_dernier_combat"] = combattant_2_nb_mois
+            
+            var_moyenne = [col for col in combat.columns if "moyenne" in col]
+
+            var_num_en_plus = [
+                "combattant_1_win",
+                "combattant_1_losses",
+                "combattant_1_age",
+                "combattant_1_la_taille",
+                "combattant_1_poids",
+                "combattant_1_reach",
+                "combattant_1_portee_de_la_jambe",
+                "combattant_2_win",
+                "combattant_2_losses",
+                "combattant_2_age",
+                "combattant_2_la_taille",
+                "combattant_2_poids",
+                "combattant_2_reach",
+                "combattant_2_portee_de_la_jambe",
+                "combattant_1_sol",
+                "combattant_1_ko_tko",
+                "combattant_1_dec",
+                "combattant_1_sub",
+                "combattant_2_sol",
+                "combattant_2_ko_tko",
+                "combattant_2_dec",
+                "combattant_2_sub",
+                "combattant_1_forme",
+                "combattant_1_serie",
+                "combattant_2_forme",
+                "combattant_2_serie",
+                "combattant_1_nb_mois_dernier_combat",
+                "combattant_2_nb_mois_dernier_combat",
+            ]
+
+            cat_features = [
+                "combattant_1_style_de_combat",
+                "combattant_1_country_of_residence_tapology",
+                "combattant_1_country_of_birth_tapology",
+                "combattant_2_style_de_combat",
+                "combattant_2_country_of_residence_tapology",
+                "combattant_2_country_of_birth_tapology",
+            ]
+            
+            liste_a_travailler = var_moyenne + var_num_en_plus + cat_features
+
+            combat = combat[liste_a_travailler]
+            
+            type_mapping = {col: 'number' if col in var_num_en_plus + var_moyenne else 'object' for col in combat.columns}
+
+            for col, dtype in type_mapping.items():
+                if dtype == 'number':
+                    combat[col] = combat[col].astype(float)  # Convertir en float pour les colonnes numériques
+                else:
+                    combat[col] = combat[col].astype(str)  # Convertir en string pour les colonnes catégorielles
+            
+            combat = _difference_num_combats(combat)
+            
+            combat.rename(columns={
+                "diff_age" : "diff_age_t",
+                "diff_win" : "diff_win_t",
+                "diff_losses" : "diff_losses_t",
+                "diff_dec" : "diff_DEC",
+                "diff_ko_tko" : "diff_KO/TKO",
+                "diff_sub" : "diff_SUB",
+                }, inplace=True)
+            
+            import joblib
+
+            model = joblib.load("./modele/LogisticRegression.pkl")
+            
+            predictions.append(model.predict_proba(combat[num_features + cat_features]))
+
+        results = ((predictions[1].flatten()[0] + predictions[0].flatten()[0])/2 , (predictions[1].flatten()[1] + predictions[0].flatten()[1])/2)
+
         def download_and_convert_image(url, filename):
             if not os.path.exists('img'):
                 os.makedirs('img')
@@ -334,7 +660,7 @@ elif st.session_state.current_page == "predictions":
             if url != "None":
                 with open(filename, 'wb') as handle:
                     response = requests.get(url, stream=True)
-                    if not response.ok:
+                    if not response.ok: 
                         print(response)
                         return None
                     for block in response.iter_content(1024):
@@ -357,7 +683,7 @@ elif st.session_state.current_page == "predictions":
             cbt1 = Image.open(image_path_1)
             cbt2  = Image.open(image_path_2)
 
-        values = [0.1, 0.9]
+        values = [results[0], results[1]]
         labels = [st.session_state["fighter_1"], st.session_state["fighter_2"]]
         imgs = [cbt1, cbt2]
 
@@ -398,7 +724,7 @@ elif st.session_state.current_page == "predictions":
                 )
             )
 
-        # Configurer le layout
+
         fig.update_layout(
             title="",
             xaxis_title="",
@@ -420,7 +746,7 @@ elif st.session_state.current_page == "predictions":
             with st.spinner('Prediction en cours'):
                 time.sleep(6)
                 st.plotly_chart(fig)
-                st.write(f"Selon l'algorrithme, {st.session_state['fighter_1']} a une probabilité de vaincre {st.session_state['fighter_2']} de {values[0]*100}%, la où {st.session_state['fighter_2']} a une probabilité de vaincre {st.session_state['fighter_1']} de {values[1]*100}%.")
+                st.write(f"Selon l'algorrithme, {st.session_state['fighter_1']} a une probabilité de vaincre {st.session_state['fighter_2']} de {round(values[0]*100)}%, la où {st.session_state['fighter_2']} a une probabilité de vaincre {st.session_state['fighter_1']} de {round(values[1]*100)}%.")
 
 
 else:
