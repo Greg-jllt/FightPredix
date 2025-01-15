608ea8807a7b22f981598f96d521f63114450f75
Hugo COCHEREAU <hugo.cochereau@hotmail.com>
2025-01-15 16:27:06 +0100

    Mise à jour des grilles de paramètres pour les modèles de machine learning : ajout de nouvelles valeurs pour le boosting, la forêt aléatoire, la régression logistique, le réseau de neurones et le SVM.

--- FightPredixML/select_best_model/grille_de_parametres.py
+++ FightPredixML/select_best_model/grille_de_parametres.py
@@ -3,53 +3,52 @@
 Ce fichier contient les grilles de paramètres pour les modèles de machine learning : boosting, random forest, regression logistique, neural network et svm.
 """
 
+import numpy as np
+
 parametres_boosting = {
-    "preprocessor__num__knn_imputer__n_neighbors": [700],
-    "feature_selection_random_forest__threshold": [0.001],
-    "boosting__n_estimators": [300],
-    "boosting__learning_rate": [0.1],
-    "boosting__subsample": [1],
-    "boosting__max_depth": [2],
-    "boosting__min_impurity_decrease": [0.7],
+    "preprocessor__num__knn_imputer__n_neighbors": [100, 400, 800],
+    "feature_selection_random_forest__threshold": [0, 0.001, 0.05],
+    "feature_selection_random_forest__estimator__n_estimators":[250],
+    "boosting__n_estimators": [100, 250, 500, 1000],
+    "boosting__learning_rate": [0.001, 0.01, 0.1],
+    "boosting__max_depth": [1,2,5]
 }
 
 parametres_random_forest = {
-    "feature_selection_random_forest__threshold": [0.001],
-    "preprocessor__num__knn_imputer__n_neighbors": [700],
+    "preprocessor__num__knn_imputer__n_neighbors": [100, 400, 800],
+    "feature_selection_random_forest__threshold": [0, 0.001, 0.05],
+    "feature_selection_random_forest__n_estimators": [250],
     "random_forest__criterion": ["entropy"],
-    "random_forest__n_estimators": [400],
-    "random_forest__min_samples_split": [2],
-    "random_forest__max_depth": [25],
-    "random_forest__min_samples_leaf": [1],
+    "random_forest__n_estimators": [200, 500, 800],
+    # "random_forest__min_samples_split": [2],
+    "random_forest__max_depth": [10, 15, 25, 30],
+    # "random_forest__min_samples_leaf": [1],
 }
 
 parametres_regression_logistique = {
-    "feature_selection_random_forest__threshold": [0.001],
-    "preprocessor__num__knn_imputer__n_neighbors": [700],
-    "regression_logistique__penalty": ["l1", "l2"],
-    "regression_logistique__C": [0.5],
-    "regression_logistique__tol": [1e-2],
-    "regression_logistique__solver": ["liblinear"],
+    "feature_selection_random_forest__threshold": [0, 0.001, 0.05],
+    "preprocessor__num__knn_imputer__n_neighbors": [250],
+    "regression_logistique__penalty": ["l1"],
+    "regression_logistique__C": [0.01, 0.1, 0.5, 1]
 }
 
 parametres_neural_network = {
-    "feature_selection_random_forest__threshold": [0.0001],
+    "feature_selection_random_forest__threshold": [0, 0.0001, 0.05],
     "feature_selection_random_forest__estimator__max_features": [84],
-    "feature_selection_random_forest__estimator__n_estimators": [200],
-    "preprocessor__num__knn_imputer__n_neighbors": [5],
-    "neural_network__hidden_layer_sizes": [(50,)],
-    "neural_network__alpha": [5],
-    "neural_network__early_stopping": [True],
-    "neural_network__validation_fraction": [0.1],
+    "feature_selection_random_forest__estimator__n_estimators": [250],
+    "preprocessor__num__knn_imputer__n_neighbors": [100,400,800],
+    "neural_network__hidden_layer_sizes": [(50,), (50,2), (80,), (150, )],
+    "neural_network__alpha": np.logspace(-5, np.log10(1/2), 20),
+    "neural_network__early_stopping": [True],   
     "neural_network__solver": ["adam"],
     "neural_network__activation": ["relu"],
 }
 
 parametres_svm = {
-    "feature_selection_random_forest__threshold": [0.0001],
+    "feature_selection_random_forest__threshold": [0, 0.0001, 0.05],
     "feature_selection_random_forest__estimator__n_estimators": [250],
     "feature_selection_random_forest__estimator__max_features": [84],
-    "preprocessor__num__knn_imputer__n_neighbors": [500],
-    "svm__C": [100],
-    "svm__gamma": [0.001],
+    "preprocessor__num__knn_imputer__n_neighbors": [100, 400, 800],
+    "svm__C": np.logspace(-3, 3, 20),
+    "svm__gamma": np.logspace(-5, np.log10(1/2), 20),
 }
